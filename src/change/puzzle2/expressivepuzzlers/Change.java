9721130b06801aa124c32cc62158e54680518374
hellojohnwoo <49154920+hellojohnwoo@users.noreply.github.com>
2019-04-02 21:17:55 +0900

    Add files via upload
    soving puzzle2

--- src/change/puzzle2/expressivepuzzlers/Change.java
+++ src/change/puzzle2/expressivepuzzlers/Change.java
@@ -1,5 +1,18 @@
+package change.puzzle2.expressivepuzzlers;
+import java.math.BigDecimal;
+
 public class Change {
     public static void main(String args[]) {
+
         System.out.println(2.00 - 1.10);
+
+        System.out.printf("%.2f%n", 2.00 - 1.10);
+
+        System.out.println((200 - 110) + " cents");
+
+        System.out.println(new BigDecimal("2.00").
+                            subtract(new BigDecimal("1.10")));
     }
+
+
 }
